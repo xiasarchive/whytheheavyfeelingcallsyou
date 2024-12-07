5a3e1c4cae73ef5f1c2d3a94d09b93f38c0bdd18
xiasarchive <xiadgarcia@gmail.com>
2024-12-07 11:07:12 -0600

    Update sketch.js

--- Calls/sketch.js
+++ Calls/sketch.js
@@ -1,111 +1,42 @@
-var stype = false;
-var atype = false;
-var vtype = false;
-var etype = false;
-var utype = false;
-var ztype = false;
-var xtype = false;
-var pressenter = true;
-var enter = false;
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <meta charset="utf-8" />
+    <title>Why The Heavy Feeling Calls You Closer To The Grounds</title>
+    <link rel="icon" type="image" href="Assets/CircleLogo.png">
+  </head>
+  <body>
+    <div class="full">
+    <div class="top">
+        <img src="Assets/TopDiv-01.png" width="700px">
 
-let open = 2;
+    </div>
 
-let img1, img2, img3, img4, img5;
-let movie1, movie2, movie3;
+    <div id="searchbar">
+        <input type="search" placeholder="surch..">
+        <button>go</button>
+    </div>
+
+    <div class="content">
+        <p id="mag">ZeebMD da Magazine</p>
+        <img src="Assets/FallingImg.png" width="190px" height="210px" id="image">
+        <p id="date">Flebruary 3006</p>
+        <hr>
+        <h3 id="magtitle">Why The <a href="Heavy/novemberGame.html">Heavy</a> Feeling <a href="Calls/index.html">Calls</a> You Closer To The <a href="Grounds/index.html">Grounds</a></h3>
+        <p id="subhead">when do you have are heavy when in the feeling on your what do when?</p>
+        <p id="author">By Fleebus Green</p>
+        <img src="Assets/BlurredText.png" width="600" height="510" id="blurredtext">
+        <form class="login" action="https://www.roblox.com">
+          <p id="view">Log In to Pew Content</p>
+          <input type="password" required id="NOVEMBERLIMAZULU" placeholder="passedwurd">
+          <input type="submit" value="lodge">
+          <p id="hint">hint: 3 wurds</p>
+      </form>
+    
+      </div>
 
 
-
-function setup() {
-  createCanvas(700, 700);
-  img1 = loadImage("data/image1.jpg");
-  img2 = loadImage("data/image2.jpg");
-  img3 = loadImage("data/image4.jpg");
-  img4 = loadImage("data/image6.jpg");
-  img5 = loadImage("data/image7.jpg");
-  movie1 = createVideo("data/image3_1.mp4");
-  movie2 = createVideo("data/image5.mp4");
-  movie3 = createVideo("data/image8.mp4");
-
-  
-
-}
-
-
-function draw(){
-background(255);
-  if (pressenter == true) {
-    background(255);
-  image(img1, 0, 0, 700, 700);
-  } else{
-  if (enter == true) {
-    pressenter = false;
-    background(255);
-    image(img2, 0, 0, 700, 700);
-  }
-  if (stype == true){
-    background(255);
-    image(movie1, 0, 0, 700, 700);
-    movie1.play();
-}
-if (atype == true){
-  movie1.stop();
-  background(255);
-  image(img3, 0, 0, 700, 700);
-}
-if (vtype == true){
-  background(255);
-  image(movie2, 0, 0, 700, 700);
-  movie2.play();
-}
-if (etype == true){
-  movie2.stop();
-  background(255);
-  image(img4, 0, 0, 700, 700);
-}
-if (utype == true){
-  background(255);
-  image(img5, 0, 0, 700, 700);
-}
-if (ztype == true){
-  background(255);
-  image(movie3, 0, 0, 700, 700);
-  movie3.play();
-}
-if (open === 3) {
-  
-  open = open + 1;
-  movie3.stop();
-  (window.open('https://xiasarchive.github.io/whytheheavyfeelingcallsyou/'));
-  
-}
-}
-
-function keyPressed(){
-  if ((keyCode === ENTER) || (keyCode === RETURN)){
-    enter = true;
-}
-if ((key == 's') || (key == 'S')){
-  stype= true; 
-}
-if ((key == 'a') || (key =='A')){
-  atype= true;
-}
-if ((key == 'v') || (key == 'V')){
-  vtype= true;
-}
-if ((key =='e') || (key == 'E')){
-  etype= true;
-}
-if ((key == 'u') || (key == 'U')){
-  utype= true;
-}
-if ((key == 'z') || (key == 'Z')){
-  ztype= true;
-}
-
-if ((key == 'x') || (key == 'X')){
-  open = open + 1;
-  
-  movie3.stop();
-}
-}
+    </div> 
+  </body>
+</html>
